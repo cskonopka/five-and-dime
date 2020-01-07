e60622b3303f4efcd6716eb650a9023a83ddbff3
Christopher Konopka <cskonopka@gmail.com>
2020-01-07 02:12:19 -0500

    created dir-gen-gif

---
+++ dir-gen-gif/dir-gen-gif.sh
@@ -0,0 +1,21 @@
+# dir-gen-gif.sh
+#!/bin/bash
+
+# Provide a directory containing video files 
+echo "Provide a directory please :"
+read dir
+
+# Specify the filetype to target
+echo "Filetype?"
+read filetype
+
+# Iterate over the directory, look for specified filetype
+for f in $dir/*.$filetype
+do
+    echo "Processing $f"
+    echo "${f%.*}.png"
+    # Generate .png analysis file
+    ffmpeg -y -ss 0 -t 11 -i $f -filter_complex "[0:v] palettegen" "${f%.*}.png"
+    # Create .gif file
+    ffmpeg -ss 0 -t 11 -i $f -filter_complex "[0:v] fps=24,scale=w=480:h=-1,split [a][b];[a] palettegen=stats_mode=single [p];[b][p] paletteuse=new=1" "${f%.*}.gif"
+done
